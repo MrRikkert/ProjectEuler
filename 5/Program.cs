b6d714bda23cf640947117718c942e8ad96d2bd3
MrRikkert <hendrikx.rik@gmail.com>
2018-06-07 21:45:41 +0200

    solution 5

---
+++ 5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Program p = new Program();
+
+			bool divised = false;
+			int i = 0;
+
+			while(!divised)
+			{
+				i++;
+				if (p.IsDivised(i))
+					divised = true;
+			}
+			Console.WriteLine(i);
+        }
+
+		private bool IsDivised(int i)
+		{
+			for(int k = 1; k <= 20; k++)
+			{
+				if (i % k != 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+    }
+}
