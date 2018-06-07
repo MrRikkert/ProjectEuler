3cd6a0ba7c5c4d6e4c6ce2c7ff0c7e4d48bb3046
MrRikkert <hendrikx.rik@gmail.com>
2018-06-07 22:01:06 +0200

    Solution 6

---
+++ 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<double> numbers = new List<double>();
+			List<double> numbers2 = new List<double>();
+			for (int i = 1; i <= 100; i++)
+			{
+				numbers.Add(Math.Pow(i, 2));
+				numbers2.Add(i);
+			}
+
+			double SumOfSquares = numbers.Sum();
+			double SquareOfSum = Math.Pow(numbers2.Sum(), 2);
+
+			Console.WriteLine(SquareOfSum - SumOfSquares);
+		}
+	}
+}
